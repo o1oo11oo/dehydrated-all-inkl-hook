b18b2362000f14818a6f155c023c591846487514
o1oo11oo <o1oo11oo@users.noreply.github.com>
2016-02-23 22:07:51 +0100

    Add basic deploy_cert function

--- hook.sh
+++ hook.sh
@@ -12,7 +12,7 @@ SCRIPTDIR="${SCRIPTDIR%%/}"
 
 function deploy_challenge {
     local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
-    local add_params='{"record_name":"_acme-challengeSUBDOMAIN","record_type":"TXT","record_data":"CHALLENGE","record_aux":"0","zone_host":"DOMAIN."}'
+    local params='{"record_name":"_acme-challengeSUBDOMAIN","record_type":"TXT","record_data":"CHALLENGE","record_aux":"0","zone_host":"DOMAIN."}'
 
     # check if challenge is for second level domain or subdomain
     SLD=$(<<<${DOMAIN} grep -oP '[^\.]+\.[^\.]+$')
@@ -20,11 +20,11 @@ function deploy_challenge {
     [[ -n ${SUBDOMAIN} ]] && SUBDOMAIN=".${SUBDOMAIN%%.}"
 
     # build request parameters
-    add_params="${add_params/SUBDOMAIN/${SUBDOMAIN}}"
-    add_params="${add_params/CHALLENGE/${TOKEN_VALUE}}"
-    add_params="${add_params/DOMAIN/${SLD}}"
+    params="${params/SUBDOMAIN/${SUBDOMAIN}}"
+    params="${params/CHALLENGE/${TOKEN_VALUE}}"
+    params="${params/DOMAIN/${SLD}}"
 
-    response="$(${SCRIPTDIR}/kasapi.sh/kasapi.sh -f "add_dns_settings" -p "${add_params}")"
+    response="$(${SCRIPTDIR}/kasapi.sh/kasapi.sh -f "add_dns_settings" -p "${params}")"
     exitval="${?}"
     [[ "${exitval}" -eq 0 ]] && sleep 10
     exit "${exitval}"
@@ -36,6 +36,16 @@ function clean_challenge {
 
 function deploy_cert {
     local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
+    local params='{"hostname":"DOMAIN","ssl_certificate_is_active":"Y","ssl_certificate_sni_key":"PRIVKEY","ssl_certificate_sni_crt":"CERT","ssl_certificate_sni_bundle":"CHAIN"}'
+
+    # build request parameters
+    params="${params/DOMAIN/${DOMAIN}}"
+    params="${params/PRIVKEY/$(echo -n $(cat ${KEYFILE} | sed 's / \\/ g' | sed ':a;N;$!ba;s/\n/\\n/g')\\n)}"
+    params="${params/CERT/$(echo -n $(cat ${CERTFILE} | sed 's / \\/ g' | sed ':a;N;$!ba;s/\n/\\n/g')\\n)}"
+    params="${params/CHAIN/$(echo -n $(cat ${CHAINFILE} | sed 's / \\/ g' | sed ':a;N;$!ba;s/\n/\\n/g')\\n)}"
+
+    response="$(${SCRIPTDIR}/kasapi.sh/kasapi.sh -f "update_ssl" -p "${params}")"
+    exit "${?}"
 }
 
 HANDLER=$1; shift; $HANDLER $@
